a3c592e5d96d85efca1d6f17a536b479706ae67a
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-11 23:34:25 -0700

    Added sections Persona and User Cases and their respective data into index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project| Phase 0</title>
+	</head>
+	<body>
+		<header>
+			<h1>Data Design Project</h1>
+			<nav>
+				<ul>
+					<li>Persona</li>
+					<li>User Story</li>
+					<li>Use Case/Interaction Flow</li>
+					<li>Conceptual Model</li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<h3>Persona</h3>
+			<p>Name: Andrew Candelaria<br>Age: 34<br>Male<br>Albuquerque, NM</p><br>
+			<p>Andrew is a 34 year old male painter who lives in downtown Albuquerque. He is surrounded by the heart and soul
+				of the city, feeling every breath and using it to fuel his art. Andrew is interested in building a community
+				of like minded artists by reading, learning and blogging about "Hispanic Art in New Mexico". He currently works at a
+				a brewery serving local beers.</p>
+			<p>Andrew loves art, and loves to be involved with all kinds or artists, but prefers to paint. He's likeable, he's social
+				and serious about his art, and serious about expressing this love online.</p>
+			<p><strong>Technology:</strong> Andrew has a Macbook that he purchased about 2 years ago. I's littered with design programs, prefers
+				Illustrator, and it has stickers of local breweries. He loves it, takes it everywhere. He can whip up a logo or print
+				in an hour.</p>
+		</section>
+		<section>
+			<h3>User Cases</h3>
+			<ul>
+				<li>As a NEW USER, I'd like to REGISTER for a PROFILE</li>
+				<li>As a registered USER, I'd like to BUILD my Profile</li>
+				<li>As an AUTHOR, I'd like to WRITE ARTICLES</li>
+				<li>As a Profile, I'd like to SEARCH TAGS</li>
+				<li>As a READER, I'd like to WRITE COMMENTS/RESPONSES</li>
+				<li>As a READER, I'd like to BOOKMARK BOOKMARKS for later use/reading</li>
+				<li>As a Profile, I'd like to CLAP</li>
+				<li>As a Profile, I'd like to SHARE Articles</li>
+			</ul>
+		</section>
+	</body>
+</html>
