a43ba8635d09dbf3ed1b0de58917ae2438b66eba
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-15 20:57:34 -0700

    added directory named sql, and a file named data-design.sql

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
 		<section>
 			<h1>Phase 1</h1>
 			<h3>Entity-Relationship Diagram</h3>
-			<img src="images/erd.png" alt="ERD image">
+			<img src="images/erd1.png" alt="ERD image">
 		</section>
 	</body>
 </html>
