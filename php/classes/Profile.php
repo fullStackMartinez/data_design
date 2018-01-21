fdfd18d78ad182e53ed9a082ba6e1f83471df191
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-21 12:10:28 -0700

    added a directory named php, and within that directory I added another directory named classes.
    Inside classes I added 4 files:
    autoloader.php
    Profile.php
    ValidateDate.php
    ValidateUuid.php

---
+++ php/classes/Profile.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Esteban
+ * Date: 1/21/2018
+ * Time: 12:04 PM
+ */
---
+++ sql/practice.sql
@@ -0,0 +1,4 @@
+INSERT INTO profile(profileId, profileName, firstName, lastName, profilePhone, profileEmail, profilePassword)
+	VALUES (unhex(replace('1abcc2e0-fc13-11e7-8450-fea9aa178066','-','')), 'thezizou','esteban','martinez','3993943388','thezizou11@gmail.com','388388');
+
+
