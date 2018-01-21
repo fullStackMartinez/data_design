fb0f509d2c1f323606b2dc6319ccd3fb9e164f05
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-21 15:43:57 -0700

    added accessor and mutator methods to property profilePhone, profileEmail, profilePassword

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -3,7 +3,7 @@
 namespace Edu\Cnm\DataDesign;
 
 require_once("autoload.php");
-require_once(dirname(__DIR__, 2) . "autoload.php");
+require_once(dirname(__DIR__) . "autoload.php");
 
 use Ramsey\Uuid\Uuid;
 
@@ -182,33 +182,98 @@ public function setProfileId( $newProfileId) : void {
 				$this->profileLastName = $newProfileLastName;
 
 				/**
-				 * accessor method for the profile Users' last name
-				 * return string value of profile last name
+				 * accessor method for the profile Users' phone number
+				 * return string value of profile phone number
 				 */
-				public function getProfileLastName() : string {
-					return ($this->profileLastName);
+				public function getProfilePhone() : string {
+					return ($this->profilePhone);
 				}
 
 				/**
-				 * mutator method for profile last name varchar 128
+				 * mutator method for profile phone number varchar 32
 				 *
-				 * @param string $newProfileLastName new value of Users' last name
-				 * @throws \InvalidArgumentException if $newProfileLastName is not a string or insecure
-				 * @throws \RangeException if $newProfileLastName is > 128 characters
-				 * @throws \TypeError if $newProfileLastName is not a string
+				 * @param string $newProfilePhone new value of Users' phone number
+				 * @throws \InvalidArgumentException if $newProfilePhone is not a string or insecure
+				 * @throws \RangeException if $newProfilePhone is > 32 characters
+				 * @throws \TypeError if $newProfilePhone is not a string
 				 */
-				public function setProfileLastName(string $newProfileLastName) : void {
-					// verify the Users' entered last name is safe
-					$newProfileLastName = trim($newProfileLastName);
-					$newProfileLastName = filter_var($newProfileLastName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-					if(empty($newProfileLastName) === true) {
-						throw(new \InvalidArgumentException("sorry, Users' last name is empty or insecure"));
+				public function setProfilePhone(string $newProfilePhone) : void {
+					// verify the Users' entered phone number is safe
+					$newProfilePhone = trim($newProfilePhone);
+					$newProfilePhone = filter_var($newProfilePhone, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+					if(empty($newProfilePhone) === true) {
+						throw(new \InvalidArgumentException("sorry, Users' phone number is empty or insecure"));
 					}
 
-					// verify the last name will fit in the database varchar128
-					if(strlen($newProfileLastName) > 128) {
-						throw(new \RangeException("sorry,Users' last name is too large"));
+					// verify the phone number will fit in the database varchar128
+					if(strlen($newProfilePhone) > 32) {
+						throw(new \RangeException("sorry, Users' phone number is too large"));
 					}
 
-					// save the Users' last name
-					$this->profileLastName = $newProfileLastName;
+					// save the Users' phone number
+					$this->profilePhone = $newProfilePhone;
+
+					/**
+					 * accessor method for the profile Users' email address
+					 * return string value of profile email
+					 */
+					public function getProfileEmail() : string {
+						return ($this->profileEmail);
+					}
+
+					/**
+					 * mutator method for profile email varchar 128
+					 *
+					 * @param string $newProfileEmail new value of Users' email
+					 * @throws \InvalidArgumentException if $newProfileEmail is not a string or insecure
+					 * @throws \RangeException if $newProfileEmail is > 128 characters
+					 * @throws \TypeError if $newProfileEmail is not a string
+					 */
+					public function setProfileEmail(string $newProfileEmail) : void {
+						// verify the Users' entered last name is safe
+						$newProfileEmail = trim($newProfileEmail);
+						$newProfileEmail = filter_var($newProfileEmail, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+						if(empty($newProfileEmail) === true) {
+							throw(new \InvalidArgumentException("sorry, Users' email is empty or insecure"));
+						}
+
+						// verify the email will fit in the database varchar128
+						if(strlen($newProfileEmail) > 128) {
+							throw(new \RangeException("sorry,Users' email is too large"));
+						}
+
+						// save the Users' email
+						$this->profileEmail = $newProfileEmail;
+
+						/**
+						 * accessor method for the profile Users' password
+						 * return string value of profile password
+						 */
+						public function getProfilePassword() : string {
+							return ($this->profilePassword);
+						}
+
+						/**
+						 * mutator method for profile password varchar 128
+						 *
+						 * @param string $newProfilePassword new value of Users' password
+						 * @throws \InvalidArgumentException if $newProfilePassword is not a string or insecure
+						 * @throws \RangeException if $newProfilePassword is > 128 characters
+						 * @throws \TypeError if $newProfilePassword is not a string
+						 */
+						public function setProfilePassword(string $newProfilePassword) : void {
+							// verify the Users' entered password is safe
+							$newProfilePassword = trim($newProfilePassword);
+							$newProfilePassword = filter_var($newProfilePassword, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+							if(empty($newProfilePassword) === true) {
+								throw(new \InvalidArgumentException("sorry, Users' password is empty or insecure"));
+							}
+
+							// verify the password will fit in the database varchar128
+							if(strlen($newProfilePassword) > 128) {
+								throw(new \RangeException("sorry, Users' password is too large"));
+							}
+
+							// save the Users' password
+							$this->profilePassword = $newProfilePassword;
+						}
