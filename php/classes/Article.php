327d5b42f4e1ab690b699026e1c83698113c8271
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-23 10:46:27 -0700

    added accessor and mutator methods for article Id, article profile Id and article content

--- php/classes/Article.php
+++ php/classes/Article.php
@@ -45,11 +45,94 @@ class Article implements \JsonSerializable {
 	private $articleTitle;
 
 	/**
-	 * accessor method for  id
+	 * accessor method for article id
 	 *
-	 * @return Uuid value of tweet id
+	 * @return Uuid value of article id
 	 **/
-	public function getTweetId() : Uuid {
-		return($this->tweetId);
+	public function getArticleId() : Uuid {
+		return($this->articleId);
 	}
+	/**
+	 * mutator method for the private property articleId
+	 *
+	 * @param Uuid|string $newArticleId new value of article id
+	 * @throws \RangeException if $newArticleId is not positive
+	 * @throws \TypeError if $newArticleId is not a uuid or string
+	 */
+
+	public function setArticleId( $newArticleId) : void {
+		try {
+			$uuid = self::validateUuid($newArticleId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the article id
+		$this->articleId = $uuid;
+	}
+
+	/**
+	 * accessor method for article profile id, the profile responsible for writing the article
+	 *
+	 * @return Uuid value of articleProfileid
+	 **/
+	public function getArticlePofileId() : Uuid {
+		return($this->articleProfileId);
+	}
+
+	/**
+	 * mutator method for the private property articleProfileId
+	 *
+	 * @param Uuid|string $newArticleProfileId new value of article profile id
+	 * @throws \RangeException if $newArticleProfileId is not positive
+	 * @throws \TypeError if $newArticleProfileId is not a uuid or string
+	 */
+
+	public function setArticleProfileId( $newArticleProfileId) : void {
+		try {
+			$uuid = self::validateUuid($newArticleProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the article profile id
+		$this->articleProfileId = $uuid;
+	}
+
+	/**
+	 * accessor method for the content of the article
+	 * return string value of content in article
+	 */
+	public function getArticleContent() : string {
+		return ($this->articleContent);
+	}
+
+	/**
+	 * mutator method for article content varchar 12000
+	 *
+	 * @param string $newArticleContent new value of article content
+	 * @throws \InvalidArgumentException if $newArticleContent is not a string or insecure
+	 * @throws \RangeException if $newArticleContent is > 12000 characters
+	 * @throws \TypeError if $newArticleContent is not a string
+	 */
+	public function setArticleContent(string $newArticleContent) : void {
+		// verify the chosen profile name is safe
+		$newArticleContent = trim($newArticleContent);
+		$newArticleContent = filter_var($newArticleContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newArticleContente) === true) {
+			throw(new \InvalidArgumentException("sorry, article content is empty or insecure"));
+		}
+
+		// verify the content of the article will fit in the database varchar12000
+		if(strlen($newArticleContent) > 12000) {
+			throw(new \RangeException("sorry, the content written exceeds the limit of characters allowed"));
+		}
+
+		// save the new content
+		$this->articleContent = $newArticleContent;
+	}
+
+
 }
