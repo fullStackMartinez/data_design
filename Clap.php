90b453e091c1b16ead8199984126f4922ee65459
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-23 12:35:37 -0700

    created Clap class

---
+++ Clap.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Edu\Cnm\DataDesign;
+require_once("autoloader.php");
+require_once(dirname(__DIR__) . "/classes/autoloader.php");
+
+use Ramsey\Uuid\Uuid;
+/**
+ * Small cross section for the entity/class of the "Medium.com" Profile
+ *
+ * This class, which is named Clap, is a template that must be followed when saving data on claps, whether it be the profile making the clap, or the article receiving a clap, on the Medium.com platform.
+ *
+ * @author Esteban Martinez <fullstackmartinez@gmail.com>
+ * @author Dylan McDonald <dmcdonald12@cnm.edu
+ * @package Edu\Cnm\Misquote
+ *
+ **/
